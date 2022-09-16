bff8d225534985cd200955607c8d71d50c3cf8bb
Amadí Thiam <amadi.thiam@gmail.com>
2022-09-16 12:55:10 -0400

    Assessment 1

---
+++ password.js
@@ -0,0 +1,4 @@
+const password = "OpenSesame10!"
+
+if (password.length >= 13) {
+} console.log(`${password.length}`);
---
+++ sdlc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This step will identify and consider the risks related to how the technology will be integrated into the standard operating procedures.  Requirements Analysis will collect the functional and system requirements of the business process, the user requirements and the operational requirements."
+const design = "The objective of this step is to transform business requirements identified during previous phases, into a detailed system architecture which is feasible, robust and brings value to the organization."
+const implementation = "This objective of this step is primarily to install the software in the production environment and to bring it into operation. It also ensures that the software, as developed, satisfies the functional requirements, satisfies the business needs, adheres to all mandates, physical constraints and service level agreements, and operates as described in User and Operator manuals."
+const testingAndIntegration = "The objective of this step is to perform system integration testing of the developed system. The systems integration test function is to ensure that the developed systems meet all the technical requirements with the components and subsystems integrated."
+const maintain = "The objective of this step is to modify and update software applications after deployments to fix bugs and improve system performance."
